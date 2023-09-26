7f616d077667a63052171f8761f088de988fd388
Felipe Spirandelli <felipereisspirandelli@gmail.com>
2023-09-25 22:58:08 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const cidades = require('./cidades.json');
+
+app.use(cors("*"));
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
